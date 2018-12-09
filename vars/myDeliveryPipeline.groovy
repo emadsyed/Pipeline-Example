266e9653dafa77d5e83e9e16d9923d36059f46d2
emadsyed <44439192+emadsyed@users.noreply.github.com>
2018-12-09 17:40:46 -0500

    Sample Template test

---
+++ vars/myDeliveryPipeline.groovy
@@ -0,0 +1,39 @@
+
+#!/usr/bin/env groovy
+def call(Map config) {
+    pipeline {
+  agent any
+stages {
+stage('Checkout'){
+  steps{
+/*
+      checkout([$class: 'GitSCM',
+            branches: [[name: env.BRANCH_NAME]],
+           extensions: [[$class: 'CleanBeforeCheckout']],
+                         userRemoteConfigs: [[url: env.REPO_NAME]] 
+                        ])
+  */
+      checkout([$class:'GitSCM', branches: "**"])
+  }
+}
+stage('Build'){
+  steps {
+    echo 'building'
+    sh 'npm install'
+  }
+}
+stage('Test'){ steps {
+    echo 'Testing'
+  
+  }
+  
+}
+stage('Publish') {
+  steps {
+   echo 'publishing' 
+  }
+}}
+}
+
+}
+
